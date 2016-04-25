81d1a819fa382608012e5a11af71036375fe6cb7
Kevin Allen <allen@uni-heidelberg.de>
2016-04-25 14:34:58 +0200

    add infrastructure to test code with testthat

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(relectro)
+
+test_check("relectro")
---
+++ tests/testthat/test_spikeTimeAutocorrelation.R
@@ -0,0 +1,6 @@
+library(relectro)
+context("SpikeTimeAutocorrelation")
+
+test_that("Sum of spikes a bin",{
+  expect_equal(1,1)
+})
