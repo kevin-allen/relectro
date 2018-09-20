49327f3038c31028c4fdb018e6dc22fea079a609
Kevin Allen <allen@uni-heidelberg.de>
2018-09-20 17:43:33 +0200

    ifrPlot function

--- R/SpikeTrain.R
+++ R/SpikeTrain.R
@@ -1226,3 +1226,124 @@ setMethod("show", "SpikeTrain",
             if(length(object@events)!=0)
               print(paste("nEvents:",length(object@events)))
                   })
+
+
+#' Plot the ifr over time of a SpikeTrain object
+#'
+#' You first need to run ifr(st) before calling this function.
+#'
+#' @param st SpikeTrain object
+#' @param cluId cluId of the cell you want to plot. cluId here is just a number
+#' @param name name for the plot
+#' @param axis.y.pos position of the y axis
+#' @param axis.x.pos position of the x axis
+#' @param axis.y.las orientation of numbers on y axis, 1 or 2
+#' @param mgp.x mgp of x axis
+#' @param mgp.y mgp of y axis
+#' @param xlab label of x axis
+#' @param ylab label of y axis
+#' @param plotxlim limits of x axis
+#' @param plotylim limits of y axis
+#' @param outma outma for graph
+#' @param margin margin of graph
+#' @param xaxis.at x axis tic location
+#' @param yaxis.at y axis tics location
+#' @param add.text text you can add on the graph
+#' @param add.text.pos position of the text you can add on the graph
+#' 
+#' @docType methods
+#' @rdname ifrPlot-methods
+setGeneric(name="ifrPlot",
+           def=function(st,cluId,
+                        name="",
+                        axis.y.pos=NA,
+                        axis.x.pos=NA,
+                        axis.y.las=2,
+                        mgp.x=c(0.5,0.1,0.1),
+                        mgp.y=c(1.1,0.2,0.1),
+                        xlab="Time (sec)",
+                        ylab="Firing rate (Hz)",
+                        plotxlim=NA,
+                        plotylim=NA,
+                        outma=c(0.5,0.5,0.5,0.5),
+                        margin=c(1.5,1.7,1,0.3),
+                        xaxis.at=NA,
+                        yaxis.at=NA,
+                        add.text="",
+                        add.text.pos=c(0,0.5))
+           {standardGeneric("ifrPlot")})
+
+#' @rdname ifrPlot-methods
+#' @aliases ifrPlot,ANY,ANY-method
+setMethod(f="ifrPlot",
+          signature = "SpikeTrain",
+          definition=function(st,cluId,
+                              name="",
+                              axis.y.pos=NA,
+                              axis.x.pos=NA,
+                              axis.y.las=2,
+                              mgp.x=c(0.5,0.1,0.1),
+                              mgp.y=c(1.1,0.2,0.1),
+                              xlab="Time (sec)",
+                              ylab="Firing rate (Hz)",
+                              plotxlim=NA,
+                              plotylim=NA,
+                              outma=c(0.5,0.5,0.5,0.5),
+                              margin=c(1.5,1.7,1,0.3),
+                              xaxis.at=NA,
+                              yaxis.at=NA,
+                              add.text="",
+                              add.text.pos=c(0,0.5))
+          {
+            if(dim(st@ifr)[1]==0)
+            {
+              stop("you need to call ifr() before calling ifrPlot()")
+            }
+            
+            if(cluId%in%st@cellList==FALSE)
+            {
+              stop(paste("cluId",cluId, "is not in the SpikeTrain object"))
+            }
+            ## get the ifr for this CluId
+            m<-st@ifr[which(st@cellList==cluId),]
+            
+            par(mar=margin, oma=outma,cex.lab=0.6,cex.axis=0.6)
+            if(any(is.na(plotxlim)))
+              plotxlim=c(min(0),max(st@ifrTime))
+            if(any(is.na(plotylim)))
+              plotylim=c(0,max(m))
+            if(any(is.na(axis.y.pos)))
+              axis.y.pos<-min(st@ifrTime)
+            if(any(is.na(axis.x.pos)))
+              axis.x.pos<-0
+            graphics::plot(x=plotxlim,y=plotylim,type='n', axes=FALSE, pch=20,lwd=1,xlab="",ylab="")
+            
+            #keep only data within xlim
+            
+            m<-m[which(st@ifrTime>=plotxlim[1]&st@ifrTime<=plotxlim[2])]
+            t<-st@ifrTime[which(st@ifrTime>=plotxlim[1]&st@ifrTime<=plotxlim[2])]
+            
+            lines(t,m)
+            par(mgp=mgp.x)
+            if(is.na(xaxis.at)){
+              graphics::axis(side = 1, pos=axis.x.pos, tck=-0.05, cex.axis=0.6)
+            }else{
+              graphics::axis(side = 1, pos=axis.x.pos, at=xaxis.at, tck=-0.05, cex.axis=0.6)
+            }
+            par(mgp=mgp.y)
+            if(is.na(yaxis.at)){
+              graphics::axis(side = 2, las=axis.y.las, pos=axis.y.pos,tck=-0.05,cex.axis=0.6)
+            } else{
+              graphics::axis(side = 2, at=yaxis.at, las=axis.y.las, pos=axis.y.pos,tck=-0.05,cex.axis=0.6)
+            }
+            graphics::title(xlab=xlab,mgp=mgp.x)
+            graphics::title(ylab=ylab,mgp=mgp.y)
+            if(name!=""){
+              graphics::title(main=name,cex.main=0.5)
+            }
+            if(add.text!=""){
+              graphics::text(labels=add.text,x=add.text.pos[1],y=add.text.pos[2],cex=0.6)
+            }
+            return()
+          }
+)
